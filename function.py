5e4ba25768baf0b6b3ef8205731103e99e1b26ac
Aliz Shrestha <98611497+thealizstha@users.noreply.github.com>
2023-11-14 19:59:41 +0545

    function

---
+++ function.py
@@ -0,0 +1,11 @@
+# function in python
+
+def display():
+  print('python function')
+display()  #function call
+
+
+#def msg():
+ #   a=input('enter name')
+  #  print('dear ',a,'welcome to python programming')
+#msg()
